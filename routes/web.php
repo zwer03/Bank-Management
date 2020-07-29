8ee50986b0f8889c80e969400758da7f2427cce8
chicoanthon-pw <anthon.chico@pointwest.com.ph>
2020-07-29 11:18:08 +0800

    cleaned up unecessary files

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('auth.login');
 });
-
-
-Route::post('register', 'userController@store');
-
-
 
 
 Route::get('/home', 'HomeController@index')->name('home');
